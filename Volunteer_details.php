0f6276eccafc06764f8a21d8210ab49c97ead164
Kapil Kukreja <kukrejakapil7@gmail.com>
2018-07-21 22:11:02 +0530

    Volunteer_Details update

--- Volunteer_details.php
+++ Volunteer_details.php
@@ -5,7 +5,7 @@
 include("db.php");
 echo "<br><br><br><center>";
 echo "<h1> Volunteers (on same Delivery)";
-$check_pro = "select User.Firstname,User.Username from User,volunteerdelivery where User.UserID=volunteerdelivery.VolunteerID";
+$check_pro = "select User.Firstname,User.Contact from User,volunteerdelivery where User.UserID=volunteerdelivery.VolunteerID";
 $result = mysqli_query($con, $check_pro); 
 if(mysqli_num_rows($result)>0){
 	
@@ -19,13 +19,33 @@ while($row = mysqli_fetch_array($result))
 {
 echo "<tr>";
 echo "<td>" . $row["Firstname"] . "</td>";
-echo "<td>" . $row["Username"] . "</td>";
+echo "<td>" . $row["Contact"] . "</td>";
+echo "</tr>";
+}
+echo "</font></table>";
+	}
+$check_pro = "select User.Firstname,User.Contact from User,volunteerdelivery,Donation where User.UserID=2";//userid of donor
+$result = mysqli_query($con, $check_pro); 
+if(mysqli_num_rows($result)>0){
+	
+echo "<table width=40% border='1' ><font size='16'>
+<tr>
+<th>Name</th>
+<th>Contact</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+echo "<tr>";
+echo "<td>" . $row["Firstname"] . "</td>";
+echo "<td>" . $row["Contact"] . "</td>";
 echo "</tr>";
 }
 echo "</font></table>";
 	}
 	
 echo "</center>"	
+
 ?>
 <html>
 	<head>
