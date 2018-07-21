eb929e0323e570266768af66b511338a86d507c5
Unknown <varunrathi1910@gmail.com>
2018-07-22 02:32:12 +0530

    Updated approve Reject functionality
    The  code deletes the entry upon rejection

--- approvereject.php
+++ approvereject.php
@@ -7,15 +7,15 @@ $db='annadhan' ;
 $data = array(); // create a variable to hold the information
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$db);
 $choice=$_POST['choice'];
-$returnstring='';
-
+$fullname=$_POST['id'];
+$names = explode(" ", $fullname);
 if(! $conn ) {
   die('Could not connect: ' . mysql_error());
 }
 
 if($choice==1)
 {
-	$sql = "UPDATE user1 SET Verfied=1" ;
+	$sql = "UPDATE user1 SET Verfied=1 where Firstname='$names[0]' and Lastname='$names[1]'" ;
 	$retval = mysqli_query($conn,$sql );
 	if(! $retval) {
 	  die('Could not get data: ' . mysqli_error());
@@ -25,7 +25,7 @@ if($choice==1)
 if($choice==2)
 {
 
-	$sql = "UPDATE user1 SET Verfied=0" ;
+	$sql = "DELETE FROM user1 where  Firstname='$names[0]' and Lastname='$names[1]'" ;
 	$retval = mysqli_query($conn,$sql );
 	if(! $retval) {
 	  die('Could not get data: ' . mysqli_error());
