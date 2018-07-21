320dd826d38f23208763d125fc0c8d75fb8b8fcb
Kapil Kukreja <kukrejakapil7@gmail.com>
2018-07-21 21:38:31 +0530

    Volunteer Details added

--- Volunteer_details.php
+++ Volunteer_details.php
@@ -4,24 +4,22 @@
 <?php 
 include("db.php");
 echo "<br><br><br><center>";
-echo "<h1> Donors (within 4 Kms)</h1>";
-$check_pro = "select User.Firstname,Donation.Food_quantity from User,Donation where User.UserID=Donation.DonorID";
+echo "<h1> Volunteers (on same Delivery)";
+$check_pro = "select User.Firstname,User.Username from User,volunteerdelivery where User.UserID=volunteerdelivery.VolunteerID";
 $result = mysqli_query($con, $check_pro); 
 if(mysqli_num_rows($result)>0){
 	
 echo "<table width=40% border='1' ><font size='16'>
 <tr>
 <th>Name</th>
-<th>Distance</th>
-<th>Quantity</th>
-<th>No. of Volunteers</th>
+<th>Contact</th>
 </tr>";
 
 while($row = mysqli_fetch_array($result))
 {
 echo "<tr>";
-echo "<td>" . $row["Firstname"] . "</td><td></td>";
-echo "<td>" . $row["Food_quantity"] . "</td><td></td>";
+echo "<td>" . $row["Firstname"] . "</td>";
+echo "<td>" . $row["Username"] . "</td>";
 echo "</tr>";
 }
 echo "</font></table>";
