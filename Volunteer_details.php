17472a2b7fa899099ba10d71c6fffede00ae9f40
Kapil Kukreja <kukrejakapil7@gmail.com>
2018-07-22 01:32:38 +0530

    Updated Queries

--- Volunteer_details.php
+++ Volunteer_details.php
@@ -3,9 +3,11 @@
 
 <?php 
 include("db.php");
+global $donor;
+$donor=1;
 echo "<br><br><br><center>";
 echo "<h1> Volunteers (on same Delivery)";
-$check_pro = "select User.Firstname,User.Contact from User,volunteerdelivery where User.UserID=volunteerdelivery.VolunteerID";
+$check_pro = "select User.Firstname,User.Contact from User,volunteerdelivery,Donation where User.UserID=volunteerdelivery.VolunteerID AND volunteerdelivery.deliveryID=Donation.DonationID AND  Donation.DonorID='$donor'";
 $result = mysqli_query($con, $check_pro); 
 if(mysqli_num_rows($result)>0){
 	
@@ -26,7 +28,7 @@ echo "</font></table>";
 	}
 echo "<br><br><br>";
 echo "Donor Details:";
-$check_pro = "select User.Firstname,User.Contact from User,volunteerdelivery,Donation where User.UserID=2";//userid of donor
+$check_pro = "select User.Firstname,User.Contact from User where User.UserID='$donor'";//userid of donor
 $result = mysqli_query($con, $check_pro); 
 if(mysqli_num_rows($result)>0){
 	
