a9b5da260363d878e79b4c615488bdc17e289fec
Unknown <varunrathi1910@gmail.com>
2018-07-22 05:33:35 +0530

    Added header for web pages

---
+++ header.php
@@ -0,0 +1,34 @@
+    <!-- no <html>,<head>,etc teags are required as they will be present in the paremt documet. -->
+
+
+    <nav class="navbar navbar-toggleable-sm bg-success navbar-inverse">
+            <div class="container">
+              <button class="navbar-toggler" data-toggle="collapse" data-target="#mainNav">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="mainNav">
+                <div class="navbar-nav">
+                  <!-- <a class="navbar-brand" href="header-nightsky.html" style="max-width:10%;border-radius:50%;"><img src="logo1.jpg" class="img-fluid"></a> -->
+                  <a class="nav-item nav-link " href="header-nightsky.php">Home</a>
+                  <a class="nav-item nav-link " href="AboutUs.php">About Us</a>
+                </div>
+              </div>
+              <div class="btn-group" style="right:0;left:auto;">
+              	<button  class="btn btn-success" onclick="relocate()">Logout</button>
+              </div>
+            </div>
+          </nav>
+      <div class="jumbotron  jumbotron-fluid text-center bg-success text-white py-1"  >
+        <h1 class = "display 1">Annadhan</h1>
+        <h4 class="display 4">Whatever's left is not over</h1>
+          <div class="collapse navbar-collapse" id="mainNav">
+          <!-- nav bar -->
+      </div>
+      <div>
+        <script type="text/javascript">
+        	function relocate()
+        	{
+        		window.location.href='logout.php';
+        	}
+        </script>
+      </div>
